6b5938dac16f27c0261ad2ed6a056e52e13bb86c
salmangujjar <salmanahmedgujjar1@gmail.com>
2022-06-10 16:07:58 +0500

    Uber EDA

---
+++ Uber/uber.R
@@ -0,0 +1,82 @@
+library(ggplot2)
+library(ggthemes)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(tidyverse) # metapackage of all tidyverse packages
+library(DT)
+library(scales)
+
+sept <- read.csv("C:/Users/Salman/Desktop/25-assments-code-in-R-main/Uber/uber-raw-data-sep14.csv")
+
+# Combine the data together 
+data <- as.data.frame(sept)
+cat("The dimensions of the data are:", dim(data))
+head(data)
+
+
+data$Date.Time <- as.POSIXct(data$Date.Time, format="%m/%d/%Y %H:%M:%S")
+data$Time <- format(as.POSIXct(data$Date.Time, format = "%m/%d/%Y %H:%M:%S"), format="%H:%M:%S")
+data$Date.Time <- ymd_hms(data$Date.Time)
+
+data$day <- factor(day(data$Date.Time))
+data$month <- factor(month(data$Date.Time, label=TRUE))
+data$year <- factor(year(data$Date.Time))
+data$dayofweek <- factor(wday(data$Date.Time, label=TRUE))
+
+
+# Add Time variables as well 
+data$second = factor(second(hms(data$Time)))
+data$minute = factor(minute(hms(data$Time)))
+data$hour = factor(hour(hms(data$Time)))
+
+
+
+# Look at the data
+head(data)
+
+
+
+hourly_data <- data %>% 
+  group_by(hour) %>% 
+  dplyr::summarize(Total = n())
+
+# Shos data in a searchable js table
+datatable(hourly_data)
+
+# Plot the data by hour
+ggplot(hourly_data, aes(hour, Total)) + 
+  geom_bar(stat="identity", 
+           fill="steelblue", 
+           color="red") + 
+  ggtitle("Trips Every Hour", subtitle = "aggregated today") + 
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5), 
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  scale_y_continuous(labels=comma)
+
+
+
+# Aggregate the data by month and hour
+month_hour_data <- data %>% group_by(month, hour) %>%  dplyr::summarize(Total = n())
+
+ggplot(month_hour_data, aes(hour, Total, fill=month)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Trips by Hour and Month") + 
+  scale_y_continuous(labels = comma)
+
+day_data <- data %>% group_by(day) %>% dplyr::summarize(Trips = n())
+day_data
+
+ggplot(day_data, aes(day, Trips)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  ggtitle("Trips by day of the month") + 
+  theme(legend.position = "none") + 
+  scale_y_continuous(labels = comma)
+day_month_data <- data %>% group_by(dayofweek, month) %>% dplyr::summarize(Trips = n())
+day_month_data
+
+ggplot(day_month_data, aes(dayofweek, Trips, fill = month)) + 
+  geom_bar(stat = "identity", aes(fill = month), position = "dodge") + 
+  ggtitle("Trips by Day and Month") + 
+  scale_y_continuous(labels = comma)
